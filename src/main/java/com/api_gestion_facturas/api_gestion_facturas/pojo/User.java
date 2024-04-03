08dbaafe143831f4dc165988fffba86cf734cf74
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2024-04-02 20:10:21 -0500

    creando conexión MySQL - Integrando modelo user

---
+++ src/main/java/com/api_gestion_facturas/api_gestion_facturas/pojo/User.java
@@ -0,0 +1,35 @@
+package com.api_gestion_facturas.api_gestion_facturas.pojo;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Data
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "numeroDeContacto")
+    private String numeroDeContacto;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "role")
+    private String role;
+}
